2d0feaa39eb8cb989fb7212f30446d83b18de714
duynguyen1711 <duynguyenn1711@gmail.com>
2025-06-19 17:08:17 +0700

    product custom & quotation custom

---
+++ addons/product_custom_info/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Product Custom Info",
+    "version": "1.0",
+    "author": "Duy Dev",
+    "category": "Product",
+    "depends": ["product"],
+    "summary": "Add custom fields to product: Thông số & Xuất xứ",
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
