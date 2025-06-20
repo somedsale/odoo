7c735905bde82250b0b3f1e251430fe7693a0950
duynguyen1711 <duynguyenn1711@gmail.com>
2025-06-20 10:17:20 +0700

    add ts,xx sale_order_line

--- addons/custom_quotation_content/models/sale_order_line.py
+++ addons/custom_quotation_content/models/sale_order_line.py
@@ -1,17 +1,7 @@
-from odoo import models, fields, api
+from odoo import fields, models
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    x_thong_so = fields.Char(string="Thông số", readonly=True)
-    x_xuat_xu = fields.Char(string="Xuất xứ", readonly=True)
-
-    @api.onchange('product_id')
-    def _onchange_product_id_custom_fields(self):
-        if self.product_id:
-            tmpl = self.product_id.product_tmpl_id
-            self.x_thong_so = tmpl.x_thong_so
-            self.x_xuat_xu = tmpl.x_xuat_xu 
-        else:
-            self.x_thong_so = 'abc'
-            self.x_xuat_xu = 'abc'
+    x_thongso = fields.Text(string='Thông số', related='product_id.product_tmpl_id.x_thong_so', store=True, readonly=True)
+    x_xuatxu = fields.Char(string='Xuất xứ', related='product_id.product_tmpl_id.x_xuat_xu', store=True, readonly=True)
--- addons/product_custom_info/models/product_template.py
+++ addons/product_custom_info/models/product_template.py
@@ -3,5 +3,5 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    x_thong_so = fields.Char(string="Thông số")
+    x_thong_so = fields.Text(string="Thông số")
     x_xuat_xu = fields.Char(string="Xuất xứ")
